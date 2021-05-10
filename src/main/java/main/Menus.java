7bd754f50e121f6a50df0cc116bcbda65b6bc5f4
Milad Nazari <milad.nazari@live.se>
2021-05-10 14:37:41 +0200

    Commented out the unused methods

--- src/main/java/main/Menus.java
+++ src/main/java/main/Menus.java
@@ -12,10 +12,10 @@ public class Menus {
 
         System.out.println("\n--|| Main menu ||--");
         System.out.println("[1] - Movie");
-        System.out.println("[2] - Actor");
-        System.out.println("[3] - Director");
-        System.out.println("[4] - Rating");
-        System.out.println("[5] - Genre");
+//        System.out.println("[2] - Actor");
+//        System.out.println("[3] - Director");
+//        System.out.println("[4] - Rating");
+//        System.out.println("[5] - Genre");
         System.out.println("[0] - Exit");
 
         System.out.print("\nMake a choice: ");
@@ -59,17 +59,17 @@ public class Menus {
     private static void movieMenu() {
         System.out.println("\n--|| Movie menu ||--");
         System.out.println("[1] - Add new movie");
-        System.out.println("[2] - Add existing actor");
-        System.out.println("[3] - Add existing director");
-        System.out.println("[4] - Add genre");
-        System.out.println("[5] - Change movie title");
-        System.out.println("[6] - Change actor");
-        System.out.println("[7] - Change director");
-        System.out.println("[8] - Delete movie");
-        System.out.println("[9] - Remove actor from movie");
-        System.out.println("[10] - Remove director from movie");
-        System.out.println("[11] - Remove genre from movie");
-        System.out.println("[12] - Show all movies");
+//        System.out.println("[2] - Add existing actor");
+//        System.out.println("[3] - Add existing director");
+//        System.out.println("[4] - Add genre");
+//        System.out.println("[5] - Change movie title");
+//        System.out.println("[6] - Change actor");
+//        System.out.println("[7] - Change director");
+//        System.out.println("[8] - Delete movie");
+//        System.out.println("[9] - Remove actor from movie");
+//        System.out.println("[10] - Remove director from movie");
+//        System.out.println("[11] - Remove genre from movie");
+//        System.out.println("[12] - Show all movies");
         System.out.println("[0] - Return to main");
 
         System.out.print("\nMake a choice: ");
@@ -131,13 +131,13 @@ public class Menus {
 
     private static void actorMenu() {
         System.out.println("\n--|| Actor menu ||--");
-        System.out.println("[1] - Add actor");
-        System.out.println("[2] - Change name");
-        System.out.println("[3] - Change age");
-        System.out.println("[4] - Change gender");
-        System.out.println("[5] - Delete actor");
-        System.out.println("[6] - Show all actors");
-        System.out.println("[0] - Return to main");
+//        System.out.println("[1] - Add actor");
+//        System.out.println("[2] - Change name");
+//        System.out.println("[3] - Change age");
+//        System.out.println("[4] - Change gender");
+//        System.out.println("[5] - Delete actor");
+//        System.out.println("[6] - Show all actors");
+//        System.out.println("[0] - Return to main");
 
         System.out.print("\nMake a choice: ");
         int choice = sc.nextInt();
@@ -172,13 +172,13 @@ public class Menus {
 
     private static void directorMenu() {
         System.out.println("\n--|| Director menu ||--");
-        System.out.println("[1] - Add director");
-        System.out.println("[2] - Change name");
-        System.out.println("[3] - Change age");
-        System.out.println("[4] - Change gender");
-        System.out.println("[5] - Delete director");
-        System.out.println("[6] - Show all directors");
-        System.out.println("[0] - Return to main");
+//        System.out.println("[1] - Add director");
+//        System.out.println("[2] - Change name");
+//        System.out.println("[3] - Change age");
+//        System.out.println("[4] - Change gender");
+//        System.out.println("[5] - Delete director");
+//        System.out.println("[6] - Show all directors");
+//        System.out.println("[0] - Return to main");
 
         System.out.print("\nMake a choice: ");
         int choice = sc.nextInt();
@@ -213,11 +213,11 @@ public class Menus {
 
     private static void ratingMenu() {
         System.out.println("\n--|| Rating menu ||--");
-        System.out.println("[1] - Add rating");
-        System.out.println("[2] - Change rating");
-        System.out.println("[3] - Delete rating");
-        System.out.println("[4] - Show all ratings");
-        System.out.println("[0] - Return to main");
+//        System.out.println("[1] - Add rating");
+//        System.out.println("[2] - Change rating");
+//        System.out.println("[3] - Delete rating");
+//        System.out.println("[4] - Show all ratings");
+//        System.out.println("[0] - Return to main");
 
         System.out.print("\nMake a choice: ");
         int choice = sc.nextInt();
@@ -246,11 +246,11 @@ public class Menus {
 
     private static void genreMenu() {
         System.out.println("\n--|| Genre menu ||--");
-        System.out.println("[1] - Add genre");
-        System.out.println("[2] - Change genre");
-        System.out.println("[3] - Delete genre");
-        System.out.println("[4] - Show all genres");
-        System.out.println("[0] - Return to main");
+//        System.out.println("[1] - Add genre");
+//        System.out.println("[2] - Change genre");
+//        System.out.println("[3] - Delete genre");
+//        System.out.println("[4] - Show all genres");
+//        System.out.println("[0] - Return to main");
 
         System.out.print("\nMake a choice: ");
         int choice = sc.nextInt();
