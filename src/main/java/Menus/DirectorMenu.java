42bef4ac3054b4ca3b8d0e779bf52f735e1db4d9
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-05-11 14:27:04 +0200

    Added change name method for Director. Added change age method for Director. Added change gender method for Director. Added delete method for Director. Added show all method for Director.

--- src/main/java/Menus/DirectorMenu.java
+++ src/main/java/Menus/DirectorMenu.java
@@ -1,6 +1,7 @@
 package Menus;
 
 import Functions.DirectorFunctions;
+import models.Director;
 
 import static main.Main.sc;
 import static main.Read.*;
@@ -15,12 +16,12 @@ public class DirectorMenu {
         while (loop) {
             System.out.println("\n--|| Director menu ||--");
             System.out.println("[1] - Add director");
-//        System.out.println("[2] - Change name");
-//        System.out.println("[3] - Change age");
-//        System.out.println("[4] - Change gender");
-//        System.out.println("[5] - Delete director");
-//        System.out.println("[6] - Show all directors");
-//        System.out.println("[0] - Return to main");
+            System.out.println("[2] - Change name");
+            System.out.println("[3] - Change age");
+            System.out.println("[4] - Change gender");
+            System.out.println("[5] - Delete director");
+            System.out.println("[6] - Show all directors");
+            System.out.println("[0] - Return to main");
 
             System.out.print("\nMake a choice: ");
             int choice = sc.nextInt();
@@ -31,19 +32,19 @@ public class DirectorMenu {
                     addDirector();
                     break;
                 case 2:
-
+                    changeName();
                     break;
                 case 3:
-
+                    changeAge();
                     break;
                 case 4:
-
+                    changeGender();
                     break;
                 case 5:
-
+                    deleteDirector();
                     break;
                 case 6:
-
+                    showAllDirectors();
                     break;
                 case 0:
                     loop = false;
@@ -52,6 +53,56 @@ public class DirectorMenu {
                     System.out.println("Wrong input!");
             }
         }
+    }
+
+    private static void deleteDirector() {
+        showAllDirectors();
+
+        System.out.println("Director id: ");
+        Long directorId = readLong();
+
+        DirectorFunctions.deleteDirector(directorId);
+
+    }
+
+    private static void changeGender() {
+        showAllDirectors();
+
+        System.out.println("Director id: ");
+        Long directorId = readLong();
+
+        String directorGender = chooseGender();
+
+        DirectorFunctions.changeGender(directorId, directorGender);
+
+    }
+
+    private static void showAllDirectors() {
+        DirectorFunctions.showAllDirectors();
+    }
+
+    private static void changeAge() {
+        showAllDirectors();
+
+        System.out.println("Director id: ");
+        Long directorId = readLong();
+
+        System.out.println("New age: ");
+        int newDirectorAge = readInt();
+
+        DirectorFunctions.changeAge(directorId, newDirectorAge);
+    }
+
+    private static void changeName() {
+        showAllDirectors();
+
+        System.out.println("Director id: ");
+        Long directorId = readLong();
+
+        System.out.println("New name: ");
+        String newDirectorName = readLine();
+
+        DirectorFunctions.changeName(directorId, newDirectorName);
     }
 
     private static void addDirector() {
@@ -63,7 +114,7 @@ public class DirectorMenu {
 
         String directorGender = chooseGender();
 
-        DirectorFunctions.addDirector(directorName,directorAge,directorGender);
+        DirectorFunctions.addDirector(directorName, directorAge, directorGender);
     }
 
 }
--- src/main/java/dao/DirectorDao.java
+++ src/main/java/dao/DirectorDao.java
@@ -18,4 +18,66 @@ public class DirectorDao {
 
         em.close();
     }
+
+    public void changeName(Long directorId, String newDirectorName) {
+        EntityManager em = emf.createEntityManager();
+
+        Director director = em.find(Director.class, directorId);
+
+        director.setName(newDirectorName);
+
+        em.getTransaction().begin();
+        em.merge(director);
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    public void changeAge(Long directorId, int newDirectorAge) {
+        EntityManager em = emf.createEntityManager();
+
+        Director director = em.find(Director.class, directorId);
+
+        director.setAge(newDirectorAge);
+
+        em.getTransaction().begin();
+        em.merge(director);
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    public void showAllDirectors() {
+        EntityManager em = emf.createEntityManager();
+
+        em.createNamedQuery("Director.findAll", Director.class).getResultList().forEach(System.out::println);
+
+        em.close();
+    }
+
+    public void changeGender(Long directorId, String directorGender) {
+        EntityManager em = emf.createEntityManager();
+
+        Director director = em.find(Director.class, directorId);
+
+        director.setGender(directorGender);
+
+        em.getTransaction().begin();
+        em.merge(director);
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    public void deleteDirector(Long directorId) {
+        EntityManager em = emf.createEntityManager();
+
+        Director director = em.find(Director.class, directorId);
+
+        em.getTransaction().begin();
+        em.remove(director);
+        em.getTransaction().commit();
+
+        em.close();
+    }
 }
