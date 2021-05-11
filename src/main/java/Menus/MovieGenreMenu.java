52dddaf3cd5b62b11c7ac1805ef76a2ddd6fdaa1
Dennis Gustafsson <dennisgust@gmail.com>
2021-05-11 14:37:15 +0200

    Loop in MovieGenreMenu

--- src/main/java/Menus/MovieGenreMenu.java
+++ src/main/java/Menus/MovieGenreMenu.java
@@ -7,36 +7,40 @@ import static main.Read.*;
 
 public class MovieGenreMenu {
     public static void genreMenu() {
+
         boolean loop = true;
-        System.out.println("\n--|| Genre menu ||--");
-        System.out.println("[1] - Add genre");
-        System.out.println("[2] - Change genre");
-        System.out.println("[3] - Delete genre");
-        System.out.println("[4] - Show all genres");
-        System.out.println("[0] - Return to main");
 
-        System.out.print("\nMake a choice: ");
-        int choice = sc.nextInt();
-        sc.nextLine();
+        while (loop) {
+            System.out.println("\n--|| Genre menu ||--");
+            System.out.println("[1] - Add genre");
+            System.out.println("[2] - Change genre");
+            System.out.println("[3] - Delete genre");
+            System.out.println("[4] - Show all genres");
+            System.out.println("[0] - Return to main");
 
-        switch (choice) {
-            case 1:
-                AddGenre();
-                break;
-            case 2:
-                ChangeGenre();
-                break;
-            case 3:
-                DeleteGenre();
-                break;
-            case 4:
-                ShowAllGenre();
-                break;
-            case 0:
-                loop = false;
-                break;
-            default:
-                System.out.println("Wrong input!");
+            System.out.print("\nMake a choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    AddGenre();
+                    break;
+                case 2:
+                    ChangeGenre();
+                    break;
+                case 3:
+                    DeleteGenre();
+                    break;
+                case 4:
+                    ShowAllGenre();
+                    break;
+                case 0:
+                    loop = false;
+                    break;
+                default:
+                    System.out.println("Wrong input!");
+            }
         }
     }
 
